bc2933546b46e01cfe20efa9c27b479bfa5a48a7
Tim van den Belt <info@timvandenbelt.com>
2021-04-17 14:28:05 +0200

    Ran prettier
    Prettier cannot handle multiple return typehints in PHP

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
-use function Symfony\Component\String\s;
 
 class PageController extends Controller
 {
@@ -34,7 +33,7 @@ class PageController extends Controller
     /**
      * Store a newly created page in storage.
      */
-    public function store(Request $request): \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+    public function store(Request $request)
     {
         $attributes = $this->validateRequest($request);
 
@@ -46,7 +45,7 @@ class PageController extends Controller
     /**
      * Display the specified page.
      */
-    public function show(Page $page): \Response|\Illuminate\Http\Response|\Inertia\Response|\Illuminate\Http\RedirectResponse
+    public function show(Page $page)
     {
         if ($page->is_draft || $page->is_private) {
             if (!Auth::check()) {
@@ -71,22 +70,22 @@ class PageController extends Controller
      * @param  \App\Models\Page  $page
      * @return \Illuminate\Http\Response|\Inertia\Response
      */
-        public function edit(Page $page)
-        {
-            //
-        }
+    //        public function edit(Page $page)
+    //        {
+    //            //
+    //        }
 
     /**
      * Update the specified resource in storage.
      */
-        public function update(Request $request, Page $page): \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-        {
-            $attributes = $this->validateRequest($request);
+    public function update(Request $request, Page $page)
+    {
+        $attributes = $this->validateRequest($request);
 
-            $page->update($attributes);
+        $page->update($attributes);
 
-            return Redirect::route('pages.edit', $page->id);
-        }
+        return Redirect::route("pages.edit", $page->id);
+    }
 
     /**
      * Remove the specified resource from storage.
@@ -99,15 +98,26 @@ class PageController extends Controller
     //        //
     //    }
 
-    private function validateRequest(Request $request): array {
+    private function validateRequest(Request $request): array
+    {
         return $request->validate([
             "title" => ["required"],
             "content" => ["required"],
             "slug" => ["required", "unique:pages"],
             "is_draft" => ["sometimes", "nullable", "boolean"],
             "is_private" => ["sometimes", "nullable", "boolean"],
-            "visible_from" => ["sometimes", "nullable","date","before:visible_until"],
-            "visible_until" => ["sometimes", "nullable","date", "after:visible_from"],
+            "visible_from" => [
+                "sometimes",
+                "nullable",
+                "date",
+                "before:visible_until",
+            ],
+            "visible_until" => [
+                "sometimes",
+                "nullable",
+                "date",
+                "after:visible_from",
+            ],
         ]);
     }
 }
